8eb8c65dbb9d0753f8afcabd9da8a581555f5440
Raywire <ryanwire@outlook.com>
2019-07-18 02:09:31 +0300

    Adds a query for authenticated user to create articles

--- app/Article.php
+++ app/Article.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'content',
+    ];
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function user()
